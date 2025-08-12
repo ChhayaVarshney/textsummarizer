94524685afba1cb7e358b402af66a6b9e579ac2d
Chhaya Varshney <chhaya3033@gmail.com>
2025-08-12 11:34:26 +0530

    Changes in model arguments

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -29,10 +29,12 @@ class ModelTrainer:
             per_device_train_batch_size=1,
             per_device_eval_batch_size=1,
             weight_decay=0.01,
-            logging_steps=10,                    
-            save_steps=500,          
+            logging_steps=10,    
+            evaluation_strategy="steps",
+            eval_steps=500,                
+            save_steps=1000,          
             save_total_limit=2,                      
-            gradient_accumulation_steps=1
+            gradient_accumulation_steps=16
         )
 
         trainer = Trainer(
